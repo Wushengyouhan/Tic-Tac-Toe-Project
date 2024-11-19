3185e4e127c0c745639c7495aeb94b6afe6e0a1b
Wushengyouhan <840835067@qq.com>
2024-11-19 21:10:20 +0800

    feat: 79. User Input & Two-Way-Binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
   console.log("Player component rendered");
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required value={name} />
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
         ) : (
-          <span className="player">{name} </span>
+          <span className="player">{playerName} </span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
